dcd6cfb702743f7152c62fbdfe1cf4fd323702ae
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-25 23:50:25 +0000

    constant moduel added

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const {IP, PORT} = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "135.23.223.133", // IP address here,
-    port: "50542"// PORT number here,
+    host: IP, // IP address here,
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
--- input.js
+++ input.js
@@ -1,10 +1,30 @@
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === '1') {
+    connection.write('Say: What\'s up');
+  }
+
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
